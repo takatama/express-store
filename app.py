c91ba7ea6377471b8690035854606096f7e6ad0f
Hirokazu Takatama <hirokazu.takatama@gmail.com>
2021-01-10 22:33:28 +0900

    ニックネームを追加。ログインを追加。

--- app.py
+++ app.py
@@ -1,16 +1,60 @@
-from bottle import route, run, template
+from bottle import route, run, template, request, redirect, response
 import sqlite3
+import uuid
+import hashlib
 
+SECRET_KEY = 'MY_HIDDEN_SECRET_KEY'
+DATABASE_FILE = 'store.db'
 
-database_file = 'store.db'
+@route('/login')
+def show_login():
+    return template('''
+<h1>ログイン</h1>
+<p style="color:red;"> {{ !message }} </p>
+<form action="/login" method="post">
+<p>メールアドレス <input name="email" type="text" placeholder="user@example.com" /></p>
+<p>パスワード <input name="password" type="password" placeholder="password" /></p>
+<p><input value="ログイン" type="submit" /></p>
+</form>''', message=request.query.message)
+
+# https://www.pythoncentral.io/hashing-strings-with-python/ より引用
+def hash_password(password):
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+    
+def is_valid_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+def query_nickname(email, password):
+    conn = sqlite3.connect(DATABASE_FILE)
+    cur = conn.cursor()
+    result = cur.execute('SELECT hashed_password, nickname FROM users WHERE email = ?;', (email,)).fetchone()
+    if result is not None and is_valid_password(result[0], password):
+        return result[1]
+    return None
+
+@route('/login', method="post")
+def do_login():
+    email = request.forms.get('email')
+    password = request.forms.get('password')
+    nickname = query_nickname(email, password)
+    if nickname is None:
+        return redirect('/login?message=ログインに失敗しました。')
+    response.set_cookie("nickname", nickname, secret=SECRET_KEY)
+    redirect('/products')
 
 @route('/products')
 def list_products():
-    conn = sqlite3.connect(database_file)
+    nickname = request.get_cookie("nickname", secret=SECRET_KEY)
+    if nickname is None:
+        redirect('/login?message=ログインしてください。')
+    conn = sqlite3.connect(DATABASE_FILE)
     cur = conn.cursor()
     results = cur.execute('SELECT * FROM products;').fetchall()
     conn.close()
     return template('''
+<p>ようこそ、{{ nickname }}さん</p>
 <h1>商品一覧</h1>
 <table border="1">
   <tr>
@@ -21,6 +65,6 @@ def list_products():
     <td>{{ product[1] }}</td><td>{{ product[2] }}</td><td><img src="{{ product[3] }}"</td><td>{{ product[4] }}円</td>
   </tr>
   %end
-</table>''', products=results)
+</table>''', nickname=nickname, products=results)
 
 run(host='localhost', port=8080)
--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ import uuid
 import hashlib
 
 
+DATABASE_FILE = 'store.db'
+
 # https://www.pythoncentral.io/hashing-strings-with-python/ より引用
 def hash_password(password):
     salt = uuid.uuid4().hex
     return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
 
-database_file = 'store.db'
-
 # append mode。もしファイルが存在していなければ作成する。
-open(database_file, 'a').close()
+open(DATABASE_FILE, 'a').close()
 
-conn = sqlite3.connect(database_file)
+conn = sqlite3.connect(DATABASE_FILE)
 cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS products;')
 cur.execute('CREATE TABLE products(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, description STRING, image_url STRING, price_yen INTEGER);')
@@ -25,8 +25,8 @@ for i in range(1, 10):
     insert_data.append(('商品' + str(i), '商品' + str(i) + 'の説明', 'https://via.placeholder.com/150', randrange(1, 100) * 100))
 cur.executemany(insert_sql, insert_data)
 cur.execute('DROP TABLE IF EXISTS users;')
-cur.execute('CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, email STRING, hashed_password STRING);')
-cur.execute('INSERT INTO users(email, hashed_password) values(?, ?)', ('user@example.com', hash_password('password')))
+cur.execute('CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, email STRING, hashed_password STRING, nickname STRING);')
+cur.execute('INSERT INTO users(email, hashed_password, nickname) values(?, ?, ?)', ('user@example.com', hash_password('password'), 'ユーザー1'))
 conn.commit()
 
 for row in cur.execute('SELECT * FROM products;').fetchall():
