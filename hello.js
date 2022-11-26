fed4d46307c89d1fda10e5f3067830de33de3afb
takatama <takatama@users.noreply.github.com>
2022-11-26 20:08:19 +0900

    added tutorial code

---
+++ hello.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const host = 'localhost'
+const port = 8080
+const ejs = require('ejs')
+app.use(express.urlencoded({ extended: true }))
+const cookieParser = require('cookie-parser')
+const secureKey = process.env.SECURE_KEY
+if (!secureKey) throw '環境変数SECURE_KEYが設定されていません。'
+app.use(cookieParser(secureKey))
+
+app.get('/hello', (req, res) => {
+  const name = req.signedCookies.name
+  const message = req.query.message
+  // これは危険なコードです！
+  res.send(ejs.render(`
+<p><%- message %></p>
+<form action="/hello" method="post">
+  <p>名前 <input type="text" name="name" value="<%= name %>"/></p>
+  <p><input type="submit"/>
+ </form>`, { name, message }))
+})
+
+app.post('/hello', (req, res) => {
+  const name = req.body.name
+  res.cookie('name', name, { httpOnly: true, signed: true })
+  res.redirect('/hello')
+})
+
+app.listen(port, host, () => {
+  console.log(`Server ready at http://${host}:${port}`)
+})
